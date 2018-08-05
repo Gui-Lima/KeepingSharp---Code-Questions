7eeceb5d29fe43077eaccb03f65fa0548587a250
Gui-Lima <ggfl@cin.ufpe.br>
2018-08-05 10:39:51 -0300

    Commented explanation

--- C++/Count Ones in a Segment/CountOnesInASegment.cpp
+++ C++/Count Ones in a Segment/CountOnesInASegment.cpp
@@ -3,13 +3,37 @@
 #include "bitset"
 using namespace std;
 
-long long pattern(long long num, int cipher) 
+
+/*      4   3   2   1   0
+ * 0    0   0   0   0   0
+ * 1    0   0   0   0   1
+ * 2    0   0   0   1   0
+ * 3    0   0   0   1   1
+ * 4    0   0   1   0   0
+ * 5    0   0   1   0   1
+ * 6    0   0   1   1   0
+ * 7    0   0   1   1   1
+ * 8    0   1   0   0   0
+ * 9    0   1   0   0   1
+ * 10   0   1   0   1   0
+ * 11   0   1   0   1   1
+ * 12   0   1   1   0   0
+ * 13   0   1   1   0   1
+ * ...
+    Notice the pattern, in the column 0, is 0 1 0 1 0 1
+                        in the column 1, is 00 11 00 11 00 11
+ *                      in the column 2, is 0000 1111 0000 1111
+ *
+ *  We will use this to count the zeros
+ */
+
+long long pattern(long long num, int cipher)
 
 {
 
-    long long bit = 1 << cipher; 
+    long long bit = 1 << cipher;
 
-    long long chain = num >> (cipher + 1); 
+    long long chain = num >> (cipher + 1);
 
     return chain * bit;
 
@@ -23,15 +47,21 @@ long long restOfBinary(long long num, int cipher)
 
     long long bit = 1 << cipher;
 
-    long long rest = num % (bit << 1); 
+    long long rest = num % (bit << 1);
 
-    return max((long long)0, rest - bit + 1);
+    return max((long long)0, (rest - bit) + 1);
 
 }
 
 
 
 long long column(long long num, int cipher) {
+    /* Go search in the column for the number of 'ones'
+     * The column we are looking at is represented by the cipher
+     * being 0 the leftmost column.
+     * So at each column we are counting the number of '1' in that column
+     * By using the pattern commented in the beginning of the code
+     */
 
     return pattern(num, cipher) + restOfBinary(num, cipher);
 
@@ -40,14 +70,15 @@ long long column(long long num, int cipher) {
 
 
 long long addUpTo(long long num) {
-    /*  
-    * 
-    * 
+    /* Here we get a number, then keeps adding the one to the total
+    *  We make Offsets starting from 0 until the number is 0, this is to check
+    *  if there are any ones left
+    *
     */
-    
+
     long long bit = 0, total = 0;
 
-    while ((num >> bit) > 0) //자릿수가 존재할때까지 각 세로줄을 다 더한다
+    while ((num >> bit) > 0)
 
         total += column(num, bit++);
 
@@ -61,7 +92,7 @@ long long countOnes(long long left, long long right) {
     //Adding all 1-digits from 0 to the highest number(right)
     //Adding all 1-digits from 0 to the lowest number-1(left-1)
     //Subctract it to obtain the real number
-        
+
     return addUpTo(right) - addUpTo(left - 1);
 
 }
@@ -72,7 +103,7 @@ int main(void)
 
 {
 
-    cout << countOnes(4, 7) << endl;
+    cout << countOnes(4, 8) << endl;
 
     return 0;
 
