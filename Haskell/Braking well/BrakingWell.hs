d964f331adfbaa6c7e9d264ddc165e2a04189638
Gui-Lima <ggfl@cin.ufpe.br>
2018-08-26 12:07:21 -0300

    New Haskell Question

---
+++ Haskell/Braking well/BrakingWell.hs
@@ -0,0 +1,9 @@
+module Codewars.G964.Braking where
+
+    dist :: Double -> Double -> Double
+    dist v mu = velocityInMS + (velocityInMS**2) / (2 * mu * 9.81)
+                where velocityInMS = (v*0.277778)
+    
+    speed :: Double -> Double -> Double
+    speed d mu =  (sqrt(((2 * d * 9.81 * mu)) + ((9.81 ** 2) * (mu ** 2))) - (9.81* mu)) * 3.6
+    
